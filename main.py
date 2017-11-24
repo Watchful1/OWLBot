1938d30f3fc6c0436baa2d6f69c6c7406df0a960
Watchful1 <watchful@watchful.gr>
2017-11-23 19:04:28 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import praw
+import os
+import logging.handlers
+import time
+import sys
+import configparser
+import signal
+
+### Config ###
+LOG_FOLDER_NAME = "logs"
+SUBREDDIT = "default"
+USER_AGENT = "OWL reddit bot (by /u/Watchful1)"
+LOOP_TIME = 15
+REDDIT_OWNER = "Watchful1"
+
+### Logging setup ###
+LOG_LEVEL = logging.DEBUG
+if not os.path.exists(LOG_FOLDER_NAME):
+	os.makedirs(LOG_FOLDER_NAME)
+LOG_FILENAME = LOG_FOLDER_NAME+"/"+"bot.log"
+LOG_FILE_BACKUPCOUNT = 5
+LOG_FILE_MAXSIZE = 1024 * 256
+
+
+log = logging.getLogger("bot")
+log.setLevel(LOG_LEVEL)
+log_formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+log_stderrHandler = logging.StreamHandler()
+log_stderrHandler.setFormatter(log_formatter)
+log.addHandler(log_stderrHandler)
+if LOG_FILENAME is not None:
+	log_fileHandler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=LOG_FILE_MAXSIZE, backupCount=LOG_FILE_BACKUPCOUNT)
+	log_formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+	log_fileHandler.setFormatter(log_formatter_file)
+	log.addHandler(log_fileHandler)
+
+
+def signal_handler(signal, frame):
+	log.info("Handling interupt")
+	quit()
+
+signal.signal(signal.SIGINT, signal_handler)
+
+log.debug("Connecting to reddit")
+
+once = False
+debug = False
+user = None
+if len(sys.argv) >= 2:
+	user = sys.argv[1]
+	for arg in sys.argv:
+		if arg == 'once':
+			once = True
+		elif arg == 'debug':
+			debug = True
+else:
+	log.error("No user specified, aborting")
+	sys.exit(0)
+
+
+try:
+	r = praw.Reddit(
+		user
+		,user_agent=USER_AGENT)
+except configparser.NoSectionError:
+	log.error("User "+user+" not in praw.ini, aborting")
+	sys.exit(0)
+
+
+async def main():
+	while True:
+		startTime = time.perf_counter()
+		log.debug("Starting run")
+
+		log.info("Logged into reddit as /u/" + str(r.user.me()))
+
+		log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
+		if once:
+			break
+
+
+
+

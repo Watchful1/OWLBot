5da2a44c63cf9cfc0bc7c5722128ebfa1d2823fd
Watchful1 <watchful@watchful.gr>
2017-11-28 23:05:57 -0800

    Move into loop

--- main.py
+++ main.py
@@ -21,19 +21,18 @@ LOOP_TIME = 15
 REDDIT_OWNER = "Watchful1"
 
 timezones = {'EST': timezone(timedelta(hours=-5)),
-			'CST': timezone(timedelta(hours=-6)),
-			'MST': timezone(timedelta(hours=-7)),
-			'PST': timezone(timedelta(hours=-8))
-			}
+             'CST': timezone(timedelta(hours=-6)),
+             'MST': timezone(timedelta(hours=-7)),
+             'PST': timezone(timedelta(hours=-8))
+             }
 
 ### Logging setup ###
 LOG_LEVEL = logging.DEBUG
 if not os.path.exists(LOG_FOLDER_NAME):
 	os.makedirs(LOG_FOLDER_NAME)
-LOG_FILENAME = LOG_FOLDER_NAME+"/"+"bot.log"
+LOG_FILENAME = LOG_FOLDER_NAME + "/" + "bot.log"
 LOG_FILE_BACKUPCOUNT = 5
 LOG_FILE_MAXSIZE = 1024 * 256
-
 
 log = logging.getLogger("bot")
 log.setLevel(LOG_LEVEL)
@@ -42,7 +41,8 @@ log_stderrHandler = logging.StreamHandler()
 log_stderrHandler.setFormatter(log_formatter)
 log.addHandler(log_stderrHandler)
 if LOG_FILENAME is not None:
-	log_fileHandler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=LOG_FILE_MAXSIZE, backupCount=LOG_FILE_BACKUPCOUNT)
+	log_fileHandler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=LOG_FILE_MAXSIZE,
+	                                                       backupCount=LOG_FILE_BACKUPCOUNT)
 	log_formatter_file = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 	log_fileHandler.setFormatter(log_formatter_file)
 	log.addHandler(log_fileHandler)
@@ -51,6 +51,7 @@ if LOG_FILENAME is not None:
 def signal_handler(signal, frame):
 	log.info("Handling interupt")
 	quit()
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -68,70 +69,64 @@ else:
 	log.error("No user specified, aborting")
 	sys.exit(0)
 
+log.debug("Connecting to reddit")
 
-# log.debug("Connecting to reddit")
-#
-#
-# try:
-# 	r = praw.Reddit(
-# 		user
-# 		,user_agent=USER_AGENT)
-# except configparser.NoSectionError:
-# 	log.error("User "+user+" not in praw.ini, aborting")
-# 	sys.exit(0)
-
-
-url = "https://overwatchleague.com/en-us/api/schedule"
 try:
-	requestTime = time.perf_counter()
-	json = requests.get(url, headers={'User-Agent': USER_AGENT})
-	requestSeconds = int(time.perf_counter() - requestTime)
-	if json.status_code != 200:
-		log.warning("Could not parse schedule, status: " + str(json.status_code))
-	stages = json.json()['data']['stages']
-except Exception as err:
-	log.warning("Could not parse schedule")
-	log.warning(traceback.format_exc())
+	r = praw.Reddit(
+		user
+		, user_agent=USER_AGENT)
+except configparser.NoSectionError:
+	log.error("User " + user + " not in praw.ini, aborting")
+	sys.exit(0)
 
-matches = []
-stageMatches = {}
-teamMatches = {}
-for stage in stages:
-	log.debug("Stage: "+str(stage['name']))
-	stageName = stage['name']
-	stageMatches[stageName] = []
-	for match in stage['matches']:
-		if match['competitors'][0] is None or match['competitors'][1] is None:
-			continue
-		game = {'home': match['competitors'][0]['abbreviatedName'],
-				'away': match['competitors'][1]['abbreviatedName'],
-				'date': datetime.datetime.strptime(match['startDate'], "%Y-%m-%dT%H:%M:%S.000Z").replace(tzinfo=timezone.utc),
-				'stage': stageName
-				}
-		matches.append(game)
-		stageMatches[stageName].append(game)
-		if game['home'] not in teamMatches:
-			teamMatches[game['home']] = []
-		if game['away'] not in teamMatches:
-			teamMatches[game['away']] = []
+while True:
+	startTime = time.perf_counter()
+	log.debug("Starting run")
 
-		teamMatches[game['home']].append(game)
-		teamMatches[game['away']].append(game)
-		log.debug("Match: "+game['home']+" vs "+game['away']+" at "+game['date'].astimezone(timezones['EST']).strftime("%m/%d %I:%M"))
+	log.info("Logged into reddit as /u/" + str(r.user.me()))
 
-for team in teamMatches:
-	log.debug(team+": "+str(len(teamMatches[team])))
+	url = "https://overwatchleague.com/en-us/api/schedule"
+	try:
+		requestTime = time.perf_counter()
+		json = requests.get(url, headers={'User-Agent': USER_AGENT})
+		requestSeconds = int(time.perf_counter() - requestTime)
+		if json.status_code != 200:
+			log.warning("Could not parse schedule, status: " + str(json.status_code))
+		stages = json.json()['data']['stages']
+	except Exception as err:
+		log.warning("Could not parse schedule")
+		log.warning(traceback.format_exc())
 
-# while True:
-# 	startTime = time.perf_counter()
-# 	log.debug("Starting run")
-#
-# 	log.info("Logged into reddit as /u/" + str(r.user.me()))
-#
-# 	log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
-# 	if once:
-# 		break
+	matches = []
+	stageMatches = {}
+	teamMatches = {}
+	for stage in stages:
+		stageName = stage['name']
+		stageMatches[stageName] = []
+		for match in stage['matches']:
+			if match['competitors'][0] is None or match['competitors'][1] is None:
+				continue
+			game = {'home': match['competitors'][0]['abbreviatedName'],
+					'away': match['competitors'][1]['abbreviatedName'],
+					'date': datetime.datetime.strptime(match['startDate'], "%Y-%m-%dT%H:%M:%S.000Z").replace(
+						tzinfo=timezone.utc),
+					'stage': stageName
+					}
+			matches.append(game)
+			stageMatches[stageName].append(game)
+			if game['home'] not in teamMatches:
+				teamMatches[game['home']] = []
+			if game['away'] not in teamMatches:
+				teamMatches[game['away']] = []
 
+			teamMatches[game['home']].append(game)
+			teamMatches[game['away']].append(game)
+			log.debug("Match: " + game['home'] + " vs " + game['away'] + " at " + game['date'].astimezone(
+				timezones['EST']).strftime("%m/%d %I:%M"))
 
+	for team in teamMatches:
+		log.debug(team + ": " + str(len(teamMatches[team])))
 
-
+	log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
+	if once:
+		break

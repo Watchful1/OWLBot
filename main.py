f83277b38b33e131c93748d4557230ffc42175b0
Watchful1 <watchful@watchful.gr>
2017-11-29 01:47:09 -0500

    Initial commit

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ import time
 import sys
 import configparser
 import signal
+import requests
+import traceback
+import datetime
+from datetime import timezone
+from datetime import timedelta
 
 ### Config ###
 LOG_FOLDER_NAME = "logs"
@@ -14,6 +19,12 @@ SUBREDDIT = "default"
 USER_AGENT = "OWL reddit bot (by /u/Watchful1)"
 LOOP_TIME = 15
 REDDIT_OWNER = "Watchful1"
+
+timezones = {'EST': timezone(timedelta(hours=-5)),
+			'CST': timezone(timedelta(hours=-6)),
+			'MST': timezone(timedelta(hours=-7)),
+			'PST': timezone(timedelta(hours=-8))
+			}
 
 ### Logging setup ###
 LOG_LEVEL = logging.DEBUG
@@ -43,8 +54,6 @@ def signal_handler(signal, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-log.debug("Connecting to reddit")
-
 once = False
 debug = False
 user = None
@@ -60,25 +69,68 @@ else:
 	sys.exit(0)
 
 
+# log.debug("Connecting to reddit")
+#
+#
+# try:
+# 	r = praw.Reddit(
+# 		user
+# 		,user_agent=USER_AGENT)
+# except configparser.NoSectionError:
+# 	log.error("User "+user+" not in praw.ini, aborting")
+# 	sys.exit(0)
+
+
+url = "https://overwatchleague.com/en-us/api/schedule"
 try:
-	r = praw.Reddit(
-		user
-		,user_agent=USER_AGENT)
-except configparser.NoSectionError:
-	log.error("User "+user+" not in praw.ini, aborting")
-	sys.exit(0)
+	requestTime = time.perf_counter()
+	json = requests.get(url, headers={'User-Agent': USER_AGENT})
+	requestSeconds = int(time.perf_counter() - requestTime)
+	if json.status_code != 200:
+		log.warning("Could not parse schedule, status: " + str(json.status_code))
+	stages = json.json()['data']['stages']
+except Exception as err:
+	log.warning("Could not parse schedule")
+	log.warning(traceback.format_exc())
 
+matches = []
+stageMatches = {}
+teamMatches = {}
+for stage in stages:
+	log.debug("Stage: "+str(stage['name']))
+	stageName = stage['name']
+	stageMatches[stageName] = []
+	for match in stage['matches']:
+		if match['competitors'][0] is None or match['competitors'][1] is None:
+			continue
+		game = {'home': match['competitors'][0]['abbreviatedName'],
+				'away': match['competitors'][1]['abbreviatedName'],
+				'date': datetime.datetime.strptime(match['startDate'], "%Y-%m-%dT%H:%M:%S.000Z").replace(tzinfo=timezone.utc),
+				'stage': stageName
+				}
+		matches.append(game)
+		stageMatches[stageName].append(game)
+		if game['home'] not in teamMatches:
+			teamMatches[game['home']] = []
+		if game['away'] not in teamMatches:
+			teamMatches[game['away']] = []
 
-async def main():
-	while True:
-		startTime = time.perf_counter()
-		log.debug("Starting run")
+		teamMatches[game['home']].append(game)
+		teamMatches[game['away']].append(game)
+		log.debug("Match: "+game['home']+" vs "+game['away']+" at "+game['date'].astimezone(timezones['EST']).strftime("%m/%d %I:%M"))
 
-		log.info("Logged into reddit as /u/" + str(r.user.me()))
+for team in teamMatches:
+	log.debug(team+": "+str(len(teamMatches[team])))
 
-		log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
-		if once:
-			break
+# while True:
+# 	startTime = time.perf_counter()
+# 	log.debug("Starting run")
+#
+# 	log.info("Logged into reddit as /u/" + str(r.user.me()))
+#
+# 	log.debug("Run complete after: %d", int(time.perf_counter() - startTime))
+# 	if once:
+# 		break
 
 
 
